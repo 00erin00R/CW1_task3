f13d8ff14b13a4ff54407eb10c0e8abe678274c6
00erin00R <erinrayworth2006@gmail.com>
2025-11-25 17:46:39 +0000

    Part B

--- task3_inventory.py
+++ task3_inventory.py
@@ -1,6 +1,36 @@
 import json
 
-with open('inventory.json', 'r') as file:
-    loaded_inventory = json.load(file)
+def load_inventory(file):
+    try:
+        with open('inventory.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("Error: 'inventory.json' file not found.")
+        return []
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from 'inventory.json'.")
+        return []
+    
+def generate_report(data):
+    total_items = sum(item.get("stock", 0) for item in data)
+    highest_price = max(data, key=lambda item: item.get("price",0))
+    out_stock = [item for item in data if item.get("stock", 0)== 0]
 
-print(loaded_inventory)
+    return total_items, highest_price, out_stock
+
+def restock_item(data, product_id):
+    id_input=input("Enter the product ID: ")
+
+
+inventory = load_inventory('inventory.json')
+total_items, highest_price, out_stock = generate_report(inventory)
+
+print("Your total stock is: ", total_items)
+print("Your highest priced item is: ", highest_price.get("name"), "at £", highest_price.get("price"))
+print("The items out of stock are: ")
+
+if out_stock:
+    for item in out_stock:
+        print("-", item.get("name"))
+else:
+    print("None")
